0cd44e16869f14d5f518ea2d4bae67f526d92320
Phael99 <raphael.alexsander99@gmail.com>
2021-08-12 17:40:13 -0300

    develop listAll method

--- src/main/java/com/phael/personApi/controller/PersonController.java
+++ src/main/java/com/phael/personApi/controller/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
@@ -24,6 +26,11 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
     }
 
 }
